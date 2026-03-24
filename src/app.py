b6f1bb87df65c4e4a0a5399fa42db2b13306f751
senai_25_26 <carolr9com@gmail.com>
2026-03-24 07:43:21 -0300

    atividade mobile

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import flet
-from flet import ThemeMode, Text, TextField, Button, Column, CrossAxisAlignment, OutlinedButton
+from flet import ThemeMode, Text, TextField, Button, Column, CrossAxisAlignment, OutlinedButton, ElevatedButton
 from flet.controls.border_radius import horizontal
+from flet.controls.material import button
 
 
 def main(page: flet.Page):
@@ -14,12 +15,25 @@ def main(page: flet.Page):
     def salvar_nome():
         text.value = f'Bom dia , {input_nome.value} {input_sobrenome.value}'
 
+    def vericar_parimpar():
+        numero = int(input_numero.value)
+        if numero % 2 == 0:
+            text_parimpar.value = f'O {numero} é par'
+        else:
+            text_parimpar.value = f'O {numero} é impar'
+
+
 
     #componentes
     text = Text()
+    text_parimpar = Text()
     input_nome = TextField(label="Nome")
     input_sobrenome = TextField(label="sobrenome")
+    input_numero= TextField(label="Digite um numero")
     btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
+    btn_verificar = ElevatedButton("Verificar", on_click=vericar_parimpar)
+
+
 
     #Construção de tela
 
@@ -28,7 +42,10 @@ def main(page: flet.Page):
             input_nome,
             input_sobrenome,
             btn_salvar,
-            text
+            text,
+            input_numero,
+            text_parimpar,
+            btn_verificar
         ],
         width=400,
         horizontal_alignment=CrossAxisAlignment.CENTER
