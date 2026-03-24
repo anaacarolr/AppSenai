2744f4caec64e55e0725f8790b9adc12238960ef
senai_25_26 <carolr9com@gmail.com>
2026-03-24 07:29:33 -0300

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,38 @@
+import flet
+from flet import ThemeMode, Text, TextField, Button, Column, CrossAxisAlignment, OutlinedButton
+from flet.controls.border_radius import horizontal
+
+
+def main(page: flet.Page):
+    #configurações
+    page.title = "Primeiro APP"
+    page.theme_mode = ThemeMode.LIGHT # Ou ThemeMode.Dark
+    page.window.width = 400
+    page.window.height = 700
+
+    #funções
+    def salvar_nome():
+        text.value = f'Bom dia , {input_nome.value}'
+
+
+    #componentes
+    text = Text()
+    input_nome = TextField(label="Nome")
+    btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
+
+    #Construção de tela
+
+    page.add(
+        Column([
+            input_nome,
+            input_nome,
+            btn_salvar,
+            text
+        ],
+        width=400,
+        horizontal_alignment=CrossAxisAlignment.CENTER
+        )
+    )
+
+flet.run(main)
+
